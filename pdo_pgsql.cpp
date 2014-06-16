0d4717eea9160c16294f078347166128e7c9e27c
Johnny Robeson <johnny@localmomentum.net>
2014-06-16 16:19:19 -0400

    fix pdo mixed indents

--- pdo_pgsql.cpp
+++ pdo_pgsql.cpp
@@ -3,30 +3,30 @@
 
 namespace HPHP {
 
-	// Initialize the extension
-	static PDOPgSql s_pgsql_driver;
+    // Initialize the extension
+    static PDOPgSql s_pgsql_driver;
 
-	PDOPgSql::PDOPgSql() : PDODriver("pgsql") {
-	}
+    PDOPgSql::PDOPgSql() : PDODriver("pgsql") {
+    }
 
-	PDOConnection *PDOPgSql::createConnectionObject() {
-		return new PDOPgSqlConnection();
-	}
+    PDOConnection *PDOPgSql::createConnectionObject() {
+        return new PDOPgSqlConnection();
+    }
 
-	long pdo_attr_lval(const Array& options, int opt, long defaultValue){
-		if(options.exists(opt)){
-			return options[opt].toInt64();
-		}
-		return defaultValue;
-	}
+    long pdo_attr_lval(const Array& options, int opt, long defaultValue){
+        if(options.exists(opt)){
+            return options[opt].toInt64();
+        }
+        return defaultValue;
+    }
 
-	String pdo_attr_strval(const Array& options, int opt, const char *def){
-		if(options.exists(opt)){
-			return options[opt].toString();
-		}
-		if(def){
-			return def;
-		}
-		return String();
-	}
+    String pdo_attr_strval(const Array& options, int opt, const char *def){
+        if(options.exists(opt)){
+            return options[opt].toString();
+        }
+        if(def){
+            return def;
+        }
+        return String();
+    }
 }
