0ee8136289cb1cb4eaaff6e83e7ceb5ffd83c890
Miquel Sabaté <mikisabate@gmail.com>
2014-11-11 07:09:25 +0100

    Let's use the FindPostgreSQL.cmake file that comes bundled with CMake.
    This way we don't have to look for libpq-fe.h or pq manually.

--- config.cmake
+++ config.cmake
@@ -1,15 +1,5 @@
-# Find include directory
-FIND_PATH(PGSQL_INCLUDE_DIR NAMES libpq-fe.h
-    PATHS /usr/include /usr/include/postgresql /usr/local/include /usr/local/include/postgresql)
 
-FIND_LIBRARY(PGSQL_LIBRARY NAMES pq PATHS /lib /usr/lib /usr/local/lib)
-
-IF (PGSQL_INCLUDE_DIR AND PGSQL_LIBRARY)
-    MESSAGE(STATUS "pgSQL Include dir: ${PGSQL_INCLUDE_DIR}")
-    MESSAGE(STATUS "libpq library: ${PGSQL_LIBRARY}")
-ELSE()
-    MESSAGE(FATAL_ERROR "Cannot find libpq library")
-ENDIF()
+find_package(PostgreSQL REQUIRED)
 
 option(HACK_FRIENDLY "Alters the API to make it work better with the Hack typechecker")
 
@@ -23,3 +13,4 @@ HHVM_EXTENSION(pgsql pgsql.cpp)
 HHVM_SYSTEMLIB(pgsql ext_pgsql.php)
 
 target_link_libraries(pgsql ${PGSQL_LIBRARY})
+
