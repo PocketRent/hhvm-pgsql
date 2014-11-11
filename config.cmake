d68733e1200123f41ac277d457bba63f92bd176b
Miquel Sabaté <mikisabate@gmail.com>
2014-11-11 08:20:43 +0100

    Pick the proper variable in CMake that points to PostgreSQL.

--- config.cmake
+++ config.cmake
@@ -12,5 +12,5 @@ include_directories(${PGSQL_INCLUDE_DIR})
 HHVM_EXTENSION(pgsql pgsql.cpp)
 HHVM_SYSTEMLIB(pgsql ext_pgsql.php)
 
-target_link_libraries(pgsql ${PGSQL_LIBRARY})
+target_link_libraries(pgsql ${PostgreSQL_LIBRARIES})
 
